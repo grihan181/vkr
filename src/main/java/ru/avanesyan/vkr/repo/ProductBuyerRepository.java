68b65ac76bee6bcfb7ff151356000fcf0df6ebae
grihan181 <osagrigoriy@mail.ru>
2024-05-24 00:58:13 +0400

    add fix for vanek 3

--- src/main/java/ru/avanesyan/vkr/repo/ProductBuyerRepository.java
+++ src/main/java/ru/avanesyan/vkr/repo/ProductBuyerRepository.java
@@ -2,12 +2,14 @@ package ru.avanesyan.vkr.repo;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import ru.avanesyan.vkr.model.Product;
 import ru.avanesyan.vkr.modelBuyer.ProductBuyer;
 
 import java.util.Optional;
 
 @RepositoryRestResource(collectionResourceRel = "productBuyer", path = "productBuyer")
+@CrossOrigin
 public interface ProductBuyerRepository extends PagingAndSortingRepository<ProductBuyer, Long> {
     ProductBuyer save(ProductBuyer entity);
 
